3770d730a215edb0aa54f8c8e15b8332d1030e9e
Sircoz <39750720+Sircoz@users.noreply.github.com>
2018-05-30 07:13:07 +0300

    Add files via upload

---
+++ meniuprincipal/meniuprincipal/Window2.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace meniuprincipal
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        private MediaPlayer mediaPlayer = new MediaPlayer();
+        int i = 1;
+
+        public Window2() => InitializeComponent();
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (i == 0)
+            {
+                i = 1;
+                butt4.Content = "🔈";
+                mediaPlayer.Pause();
+            }
+            else
+            {
+                butt4.Content = "🔉";
+                i = 0;
+                mediaPlayer.Play();
+            }
+
+        }
+        private void butt5_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                mediaPlayer.Open(new Uri(openFileDialog.FileName));
+                mediaPlayer.Play();
+            }
+        }
+
+    }
+}
